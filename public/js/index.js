736b555eb9954a53a2ec381790fbf4b13a328915
Adrian_G <adriangarcia.comunicacion@gmail.com>
2023-02-02 22:10:27 +0100

    Add app class and index synconisation

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,3 @@
-"use strict";
+import App from './components/App/App.js';
+const app = new App(document.body);
+app.render();
--- src/components/App/App.ts
+++ src/components/App/App.ts
@@ -1,15 +1,18 @@
+// Import Card from '../Card/Card.js';
 import Component from '../Component.js';
+import Header from '../Header/Header.js';
 
 export default class App extends Component {
   #children: Component[];
 
   constructor(parentElement: HTMLElement | null) {
     super(parentElement, 'div');
-    this.#children = [];
+    this.#children = [new Header('Mi título', this.element, 'main-header')];
   }
 
   render(): void {
     super.render();
     this.#children.forEach(component => component.render());
+    this.element.className = 'container';
   }
 }
